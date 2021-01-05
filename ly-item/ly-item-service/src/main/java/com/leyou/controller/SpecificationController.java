4e0f26eb4c01f3c2904bd4710fca23e0269dfaf6
jinyuxin <jinyuxin0326@163.com>
2021-01-05 08:55:32 +0800

    [金余新]完成规格参数组、规格参数的增加|修改|删除

--- ly-item/ly-item-service/src/main/java/com/leyou/controller/SpecificationController.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/controller/SpecificationController.java
@@ -21,6 +21,9 @@ public class SpecificationController {
     @Autowired
     private SpecificationService specificationService;
 
+    /**
+     * 查询规格参数组
+     * */
     @GetMapping("groups/{cid}")
     public ResponseEntity<List<SpecGroup>> querySpecGroups(@PathVariable("cid") Long cid){
         List<SpecGroup> list = this.specificationService.querySpecGroups(cid);
@@ -30,13 +33,72 @@ public class SpecificationController {
         return ResponseEntity.ok(list);
     }
 
+
+    /**
+     * 新增规格参数组
+     * */
+    @PostMapping("group")
+    public ResponseEntity<Void> addSpecGroup(@RequestBody SpecGroup group){
+        this.specificationService.addSpecGroup(group);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * 编辑规格参数组
+     * */
+    @PutMapping("group")
+    public ResponseEntity<Void> editSpecGroup(@RequestBody SpecGroup group){
+        this.specificationService.editSpecGroup(group);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * 删除规格参数组
+     * */
+    @DeleteMapping("group/{gid}")
+    public ResponseEntity<Void> deleteSpecGroup(@PathVariable(value="gid", required = false) Long gid){
+        this.specificationService.delSpecGroup(gid);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * 查询规格参数
+     * */
     @GetMapping("/params")
     public ResponseEntity<List<SpecParam>> querySpecParam(@RequestParam(value="gid", required = false) Long gid){
         List<SpecParam> list =
                 this.specificationService.querySpecParams(gid);
-        if(list == null || list.size() == 0){
+        if(list == null || list.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return ResponseEntity.ok(list);
     }
+
+    /**
+     * 增加规格参数
+     * */
+    @PostMapping("param")
+    public ResponseEntity<Void> addSpecParam(@RequestBody SpecParam specParam){
+        this.specificationService.addSpecParam(specParam);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * 编辑规格参数
+     * */
+    @PutMapping("param")
+    public ResponseEntity<Void> editSpecParam(@RequestBody SpecParam specParam){
+        this.specificationService.editSpecParam(specParam);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * 删除规格参数
+     * */
+    @DeleteMapping("param/{id}")
+    public ResponseEntity<Void> deleteSpecParam(@PathVariable(value="id", required = false) Long id){
+        this.specificationService.delSpecParam(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- ly-item/ly-item-service/src/main/java/com/leyou/service/SpecificationService.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/service/SpecificationService.java
@@ -4,6 +4,8 @@ import com.leyou.item.pojo.SpecGroup;
 import com.leyou.item.pojo.SpecParam;
 import com.leyou.mapper.SpecGroupMapper;
 import com.leyou.mapper.SpecParamMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,9 @@ import java.util.List;
  */
 @Service
 public class SpecificationService {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(SpecificationService.class);
+
     @Autowired
     private SpecGroupMapper specGroupMapper;
 
@@ -32,4 +37,41 @@ public class SpecificationService {
         t.setGroupId(gid);
         return this.specParamMapper.select(t);
     }
+
+    public void addSpecGroup(SpecGroup specGroup) {
+        LOGGER.info("开始规格组增加服务");
+        this.specGroupMapper.insert(specGroup);
+        LOGGER.info("结束规格组增加服务");
+    }
+
+    public void editSpecGroup(SpecGroup specGroup) {
+        LOGGER.info("开始规格组编辑服务");
+        this.specGroupMapper.updateByPrimaryKey(specGroup);
+        LOGGER.info("结束规格组编辑服务");
+    }
+
+    public void delSpecGroup(Long gid) {
+        LOGGER.info("开始规格组删除服务");
+        this.specGroupMapper.deleteByPrimaryKey(gid);
+        LOGGER.info("结束规格组删除服务");
+    }
+
+    public void addSpecParam(SpecParam specParam) {
+        LOGGER.info("开始规格参数增加服务");
+        this.specParamMapper.insert(specParam);
+        LOGGER.info("结束规格参数增加服务");
+    }
+
+    public void editSpecParam(SpecParam specParam) {
+        LOGGER.info("开始规格参数编辑服务");
+        this.specParamMapper.updateByPrimaryKey(specParam);
+        LOGGER.info("结束规格参数编辑服务");
+    }
+
+    public void delSpecParam(Long id) {
+        LOGGER.info("开始规格参数删除服务");
+        this.specParamMapper.deleteByPrimaryKey(id);
+        LOGGER.info("结束规格参数删除服务");
+    }
+
 }
